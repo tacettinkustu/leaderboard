4660651fd1a1523f2c626927f211b6f1f5121521
tacettinkustu <tacettinkustu>
2021-07-25 23:12:39 +0300

    Create main template

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
 import './style.css';
-
-function component() {
-  const element = document.createElement('div');
-
-  element.innerHTML = 'Welcome to Microverse World';
-  element.classList.add('hello');
-
-  return element;
-}
-
-document.body.appendChild(component());
