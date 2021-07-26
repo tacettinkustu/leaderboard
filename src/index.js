bd0cdfd43d9de7715004fd430c63cb548dad54b5
tacettinkustu <tacettinkustu>
2021-07-26 16:53:30 +0300

    Start game with posting game name

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
+import Leaderboard from './js/leaderboard.js';
 import './style.css';
+
+const addForm = document.querySelector('form');
+// const nameInput = document.querySelector('#name');
+// const scoreInput = document.querySelector('score');
+// const refresh = document.querySelector('button');
+const leaderboard = new Leaderboard();
+
+function startGame() {
+  leaderboard
+    .startGame('futbol')
+    .then((response) => response.result.split(' '))
+    .then((res) => console.log(res[3]))
+    .catch((err) => console.err(err));
+}
+
+function postScore() {
+  // post scores to API
+}
+
+document.addEventListener('DOMContentLoaded', startGame);
+addForm.addEventListener('submit', postScore);
---
+++ src/js/leaderboard.js
@@ -0,0 +1,22 @@
+class Leaderboard {
+  constructor() {
+    this.url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+  }
+
+  async startGame(gameName) {
+    const responseStart = await fetch(this.url, {
+      method: 'POST',
+      body: JSON.stringify({
+        name: gameName,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+
+    const responseData = await responseStart.json();
+    return responseData;
+  }
+}
+
+export default Leaderboard;
